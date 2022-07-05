52a9f79529a7465607232ace95ea583c26817c5c
Nuh Aslan <107987193+Nslan@users.noreply.github.com>
2022-07-05 23:17:55 +0300

    1-100 arası asal sayılar

---
+++ AsalSayılar.java
@@ -0,0 +1,33 @@
+package Giris;
+
+public class AsalSayılar {
+
+    public static void main(String[] args) {
+
+
+        int count=0;
+        for(int sayi=2;sayi<=100;sayi++){
+
+            boolean control = false;
+            for ( int j=2; j<sayi; j++){
+
+                if ( sayi % j == 0 ){//sayi birkez bile bolunebiliyorsa true dondur ve artık kontrol yapma
+                    control = true ;
+                    break;
+                }else{
+                    control = false;
+                }
+
+            }
+            if (control == false){
+                System.out.print(sayi+" ");
+                count++;
+            }
+
+        }
+
+
+
+
+    }
+}
